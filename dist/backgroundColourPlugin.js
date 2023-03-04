0c71a8c0c7d91925fc1f672849fcf95ebed04c89
Kim <46893395+kalleriakronos24@users.noreply.github.com>
2023-03-04 11:27:25 +0700

    make validation on canvas object

--- dist/backgroundColourPlugin.js
+++ dist/backgroundColourPlugin.js
@@ -1,22 +1,26 @@
 const { Chart } = require('chart.js');
 
 class BackgroundColourPlugin {
- 	id = 'chartjs-plugin-chartjs-node-canvas-background-colour';
+    id = 'chartjs-plugin-chartjs-node-canvas-background-colour';
+    constructor(
+        _width,
+        _height,
+        _fillStyle
+    ) { 
+        this._fillStyle = _fillStyle,
+        this._width = _width,
+        this._height = _height
+    }
 
- 	constructor(
- 		_width,
- 		_height,
- 		_fillStyle
- 	) { }
+    beforeDraw(chart) {
+        if (chart.canvas) {
+            const ctx = chart.canvas.context
+            ctx.save();
+            ctx.fillStyle = this._fillStyle;
+            ctx.fillRect(0, 0, this._width, this._height);
+            ctx.restore();
+        }
+    }
+}
 
- 	beforeDraw(chart) {
-
- 		const ctx = chart.ctx;
- 		ctx.save();
- 		ctx.fillStyle = this._fillStyle;
- 		ctx.fillRect(0, 0, this._width, this._height);
- 		ctx.restore();
- 	}
- }
-
- exports.BackgroundColourPlugin = BackgroundColourPlugin;
+exports.BackgroundColourPlugin = BackgroundColourPlugin;
